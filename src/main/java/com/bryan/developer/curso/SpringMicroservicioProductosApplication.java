49ba59112567cc424a10511fad2a078dac5f0667
Bryan Antonio Alvarado Villalobos <bryan.developer.alvarado@gmail.com>
2020-03-19 20:01:03 -0500

    Agregando base del repositorio

---
+++ src/main/java/com/bryan/developer/curso/SpringMicroservicioProductosApplication.java
@@ -0,0 +1,13 @@
+package com.bryan.developer.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservicioProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicioProductosApplication.class, args);
+	}
+
+}
